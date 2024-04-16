4d195d093e2461ac63ad774a44a36ff95d534b19
Colin Bate <colin@colinbate.com>
2024-04-16 13:09:12 -0300

    Make home page more useful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-	import { Button } from "$ui/button";
+	import { Button } from '$ui/button';
 </script>
-<header class="bg-primary px-4 py-2 font-game">
-  <h1 class="text-3xl">Eldergrove</h1>
-</header>
-<div class="p-8 flex gap-4">
-  <Button href="/play/">Play New Game</Button>
-  <Button>Login</Button>
-</div>
+
+<main class="flex flex-col items-center">
+	<header class="w-full max-w-6xl">
+		<img src="/img/eldergrove-banner.webp" alt="Eldergrove" />
+	</header>
+	<div class="w-full max-w-6xl p-8">
+		If you are here, then thank you for agreeing to help test Eldergrove. It is very early and there
+		are many issues already known. The code and issues are available here:
+		<a class="block pt-4 text-emerald-300" href="https://github.com/ttrpgtools/eldergrove"
+			>https://github.com/ttrpgtools/eldergrove</a
+		>
+	</div>
+	<div class="flex w-full max-w-6xl gap-4 p-8">
+		<Button href="/game/test">Play New Game (Pre-Alpha)</Button>
+		<Button><del>Login</del></Button>
+	</div>
+</main>
